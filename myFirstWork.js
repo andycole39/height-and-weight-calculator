39a3f1bdf8309175fdc36b3960738a89b2285daf
yakubu andrew <105059452+andycole39@users.noreply.github.com>
2022-06-16 20:40:02 +0100

    Add files via upload

---
+++ myFirstWork.js
@@ -0,0 +1,7 @@
+//function
+function calc() {
+    let w = document.getElementById("width").value;
+let  h = document.getElementById("height").value;
+    let area = w * h;
+    document.getElementById("answer").value=area;
+}
